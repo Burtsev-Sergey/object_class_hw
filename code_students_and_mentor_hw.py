0fe63551e1e5a6a727bf5fb4482b70059af854b6
Burtsev Sergey <sburtsevs@mail.ru>
2024-09-29 23:39:58 +0300

    Task 2_ Attributes and class interactions. To class Reviewer added method give students grades for hw _ to class Student added method give lecturers grades for lectures

--- code_students_and_mentor_hw.py
+++ code_students_and_mentor_hw.py
@@ -1,11 +1,21 @@
 class Student:
     def __init__(self, name, surname, gender):
+        # print('Student init is called!')
         self.name = name
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.lecturer_grades:
+                lecturer.lecturer_grades[course] += [grade]
+            else:
+                lecturer.lecturer_grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
@@ -13,6 +23,16 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        # print('Lecturer init is called!')
+        self.lecturer_grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        # print('Reviewer init is called!')
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,27 +42,27 @@ class Mentor:
         else:
             return 'Ошибка'
         
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        print('Lecturer init is called!')
 
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        print('Reviewer init is called!')
-
-lecturer_call = Lecturer('Ivan', 'Petrov')
-reviewer_call = Reviewer('Petr', 'Ivanov')
- 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
+
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-# print(best_student.grades)
+
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_hw(cool_lecturer, 'Python', 10)
+best_student.rate_hw(cool_lecturer, 'Python', 9)
+best_student.rate_hw(cool_lecturer, 'Python', 8)
+
+print(cool_lecturer.lecturer_grades)
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
