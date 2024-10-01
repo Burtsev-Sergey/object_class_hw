3d4bac959a4cdd178b3c729bcdf75ec9fc3fc99a
Burtsev Sergey <sburtsevs@mail.ru>
2024-10-01 15:15:32 +0300

    Task 3_Polymorphism and magic methods. Added a comparison of lecturers and students by average grade

--- code_students_and_mentor_hw.py
+++ code_students_and_mentor_hw.py
@@ -1,6 +1,5 @@
 class Student:
     def __init__(self, name, surname, gender):
-        # print('Student init is called!')
         self.name = name
         self.surname = surname
         self.gender = gender
@@ -8,7 +7,7 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_hw(self, lecturer, course, grade):
+    def rate_lecturers(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.lecturer_grades:
                 lecturer.lecturer_grades[course] += [grade]
@@ -24,6 +23,11 @@ class Student:
         if not all_grades:
             return 0
         return sum(all_grades) / len(all_grades)
+    
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self.get_average_grade() < other.get_average_grade()
 
     def __str__(self) -> str:
         print_courses_in_progress = ', '.join(self.courses_in_progress)
@@ -43,7 +47,6 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        # print('Lecturer init is called!')
         self.lecturer_grades = {}
 
     def get_average_grade(self):
@@ -53,6 +56,11 @@ class Lecturer(Mentor):
         if not all_grades:
             return 0
         return sum(all_grades) / len(all_grades)
+    
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self.get_average_grade() < other.get_average_grade()
 
     def __str__(self) -> str:
         average_grade = round(self.get_average_grade(), 1)
@@ -61,7 +69,6 @@ class Lecturer(Mentor):
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        # print('Reviewer init is called!')
 
     def __str__(self) -> str:
         return f"\nПроверяющий\n\nИмя: {self.name}\nФамилия: {self.surname}\n"
@@ -80,20 +87,35 @@ some_student = Student('Ruoy', 'Eman', 'your_gender')
 some_student.courses_in_progress += ['Python', 'Git']
 some_student.finished_courses += ['Введение в программирование']
 
+some_student1 = Student('Ivan', 'Belov', 'your_gender')
+some_student1.courses_in_progress += ['Python', 'Git']
+some_student1.finished_courses += ['Введение в программирование']
+
 some_mentor = Mentor('Some', 'Buddy')
 some_mentor.courses_attached += ['Python']
+
+some_mentor1 = Mentor('Mark', 'Grizzly')
+some_mentor1.courses_attached += ['Git']
 
 some_lecturer = Lecturer('Some', 'Buddy')
 some_lecturer.courses_attached += ['Python']
 
-some_student.rate_hw(some_lecturer, 'Python', 10)
-some_student.rate_hw(some_lecturer, 'Python', 9)
-some_student.rate_hw(some_lecturer, 'Python', 10)
+some_lecturer1 = Lecturer('Mark', 'Grizzly')
+some_lecturer1.courses_attached += ['Git']
 
-# print(some_lecturer.lecturer_grades)
+some_student.rate_lecturers(some_lecturer, 'Python', 10)
+some_student.rate_lecturers(some_lecturer, 'Python', 9)
+some_student.rate_lecturers(some_lecturer, 'Python', 10)
+
+some_student1.rate_lecturers(some_lecturer1, 'Git', 9)
+some_student1.rate_lecturers(some_lecturer1, 'Git', 9)
+some_student1.rate_lecturers(some_lecturer1, 'Git', 9)
 
 some_reviewer = Reviewer('Some', 'Buddy')
 some_reviewer.courses_attached += ['Python']
+
+some_reviewer1 = Reviewer('Petr', 'Sidorof')
+some_reviewer1.courses_attached += ['Git']
 
 some_reviewer.rate_hw(some_student, 'Python', 10)
 some_reviewer.rate_hw(some_student, 'Python', 10)
@@ -103,9 +125,29 @@ some_reviewer.rate_hw(some_student, 'Python', 10)
 some_reviewer.rate_hw(some_student, 'Python', 10)
 some_reviewer.rate_hw(some_student, 'Python', 10)
 
-print(some_reviewer)
-print(some_lecturer)
-print(some_student)
-print(some_mentor)
+some_reviewer1.rate_hw(some_student1, 'Git', 10)
+some_reviewer1.rate_hw(some_student1, 'Git', 10)
+some_reviewer1.rate_hw(some_student1, 'Git', 10)
 
-# print(some_student.grades)
+print(some_reviewer)
+print(some_reviewer1)
+print(some_lecturer)
+print(some_lecturer1)
+print(some_student)
+print(some_student1)
+# print(some_mentor)
+# print(some_mentor1)
+
+students = [some_student, some_student1]
+lecturers = [some_lecturer, some_lecturer1]
+
+sorted_students = sorted(students, reverse=True)
+sorted_lecturers = sorted(lecturers, reverse=True)
+
+print("Студенты, упорядоченные по средней оценке за домашние задания (убывание):\n")
+for student in sorted_students:
+    print(student)
+
+print("Лекторы, упорядоченные по средней оценке за лекции (убывание):\n")
+for lecturer in sorted_lecturers:
+    print(lecturer)
